2a2f97e4ea3b4ee92bdc95d7079bca08790bb257
Edwin Fernando <ef322@ic.ac.uk>
2025-06-07 20:07:00 +0100

    feat: interface frost signing with mina verification

--- frost-bluepallas/src/translate.rs
+++ frost-bluepallas/src/translate.rs
@@ -1,22 +1,19 @@
-// Extracting the Element out of the VerifyingKey Struct with some serde magic
-// How to unit test it? Generate a VerifyingKey from a SigningKey from a scalar (arkworks) on the
-//     frost side
-// Do the same on the mina side
-//
-
 use crate::PallasPoseidon;
 use ark_ec::short_weierstrass::{Affine, Projective};
-use frost_core::{Ciphersuite, Group, Signature as FrSig, VerifyingKey};
+use ark_ec::AffineRepr;
+use frost_core::{Ciphersuite, Group, Scalar, Signature as FrSig, VerifyingKey}; // Fr for frost
 use mina_curves::pasta::PallasParameters;
-use mina_signer::{pubkey::PubKey, signature::Signature as MinaSig};
+use mina_signer::{pubkey::PubKey, signature::Signature as MinaSig, NetworkId};
+use mina_hasher::{Hashable, ROInput};
 
 // temporary till we sort out proper error messages
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 // Note
-// CurvePoint = Affine<PallasParameters>                      mina side
-// PallasProjective = Projective<PallasParameters>            frost side
+// CurvePoint = Affine<PallasParameters>                                        mina side
+// PallasProjective = Projective<PallasParameters> (= Element<PallasPoseidon>)  frost side
+// The ScalarField type on the mina and frost side are the same!
 
-fn translate_pk(fr_pk: VerifyingKey<PallasPoseidon>) -> Result<PubKey> {
+pub fn translate_pk(fr_pk: &VerifyingKey<PallasPoseidon>) -> Result<PubKey> {
     // A VerifyingKey is just a group element in some wrapper structs
     // But the api doesn't seem to expose a way to extract the underlying element
     // So I serialize VerifyingKey and deserialize into Element
@@ -35,13 +32,54 @@ fn translate_pk(fr_pk: VerifyingKey<PallasPoseidon>) -> Result<PubKey> {
     Ok(PubKey::from_point_unsafe(pk_affine))
 }
 
-fn translate_sig(fr_sig: FrSig<PallasPoseidon>) -> MinaSig {
-    unimplemented!()
+pub fn translate_sig(fr_sig: &FrSig<PallasPoseidon>) -> Result<MinaSig> {
+    let r: &Projective<PallasParameters> = fr_sig.R();
+    let z: &Scalar<PallasPoseidon> = fr_sig.z();
+    let r_affine: Affine<PallasParameters> = r.clone().into(); // Is this step required?
+    let (rx, _ry) = r_affine
+        .xy()
+        .ok_or("nonce commitment is the point at infinity??!!")?;
+
+    Ok(MinaSig {
+        rx: rx.clone(),
+        s: z.clone(),
+    })
 }
+
+
+
+// This s temporary: it's easier to test that u8 slices as messages work correctly first
+// We can implmement (or reuse) the Hashable trait for transaction as in this example afterwards:
+// https://github.com/o1-labs/proof-systems/blob/master/signer/README.md?plain=1#L19-L40
+
+
+#[derive(Clone, Debug)]
+pub struct PallasMessage (pub Vec<u8>);
+
+// Implement a hashable trait for a u8 slice
+impl Hashable for PallasMessage {
+    type D = NetworkId;
+
+    fn to_roinput(&self) -> ROInput {
+        ROInput::new().append_bytes(self.0.as_ref())
+    }
+
+    // copied from
+    // https://github.com/o1-labs/proof-systems/blob/0.1.0/signer/tests/transaction.rs#L53-L61
+    fn domain_string(network_id: NetworkId) -> Option<String> {
+        // Domain strings must have length <= 20
+        match network_id {
+            NetworkId::MAINNET => "MinaSignatureMainnet",
+            NetworkId::TESTNET =>"FROST-PALLAS-POSEIDON",// "CodaSignature",
+        }
+        .to_string()
+        .into()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
     use ark_ff::fields::models::fp::{Fp, MontBackend};
     use frost_core::SigningKey;
@@ -54,8 +92,9 @@ mod tests {
         // Then use the translation function to check if it's the same element on both sides
 
         // The type of Scalar from which a SecretKey can be made (on Mina side): Fp<MontBackend<FrConfig, 4>, 4>
-        let n: u32 = 57639753; // generate loads of random n and test
-                               // <PallasParameters as CurveConfig>::ScalarField is the same type as Fp<...>
+        let n: u32 = 57639753; // TODO generate loads of random n and test
+        
+        // <PallasParameters as CurveConfig>::ScalarField is the same type as Fp<...>
         let scalar: Fp<MontBackend<FrConfig, 4>, 4> = Fp::new(n.into());
         let mina_sk = SecKey::new(scalar);
         let mina_pk = PubKey::from_secret_key(mina_sk)?;
@@ -64,7 +103,7 @@ mod tests {
         let fr_sk = SigningKey::from_scalar(scalar)?;
         let fr_pk: VerifyingKey<PallasPoseidon> = fr_sk.into();
 
-        assert_eq!(translate_pk(fr_pk)?, mina_pk);
+        assert_eq!(translate_pk(&fr_pk)?, mina_pk);
         Ok(())
     }
 }
