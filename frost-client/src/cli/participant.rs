0909fb2cbc71aa96b9fa3988d800f51e13ca5359
jCabala <kuba.cabala6@gmail.com>
2025-06-26 23:38:20 +0100

    feat: Updated outter-most cli

--- frost-client/src/cli/participant.rs
+++ frost-client/src/cli/participant.rs
@@ -4,13 +4,12 @@ use std::rc::Rc;
 use eyre::eyre;
 use eyre::Context;
 use eyre::OptionExt;
-use reddsa::frost::redpallas::PallasBlake2b512;
 use reqwest::Url;
 
 use frost_core::keys::KeyPackage;
 use frost_core::Ciphersuite;
-use frost_ed25519::Ed25519Sha512;
 use frost_rerandomized::RandomizedCiphersuite;
+use frost_bluepallas::PallasPoseidon;
 
 use super::{args::Command, config::Config};
 
@@ -26,10 +25,8 @@ pub async fn run(args: &Command) -> Result<(), Box<dyn Error>> {
 
     let group = config.group.get(&group).ok_or_eyre("Group not found")?;
 
-    if group.ciphersuite == Ed25519Sha512::ID {
-        run_for_ciphersuite::<Ed25519Sha512>(args).await
-    } else if group.ciphersuite == PallasBlake2b512::ID {
-        run_for_ciphersuite::<PallasBlake2b512>(args).await
+    if group.ciphersuite == PallasPoseidon::ID {
+        run_for_ciphersuite::<PallasPoseidon>(args).await
     } else {
         Err(eyre!("unsupported ciphersuite").into())
     }
