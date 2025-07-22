2595b99e6d76771772b59f29e89a6475aa706288
Edwin Fernando <ef322@ic.ac.uk>
2025-07-22 18:12:47 +0200

    feat: porting parts of interoperability and integration tests

---
+++ frost-bluepallas/tests/interoperability_tests.rs
@@ -0,0 +1,16 @@
+use rand_core::SeedableRng;
+
+mod helpers;
+
+#[test]
+fn check_interoperability_in_sign_with_dealer() {
+    // Test with multiple keys/signatures to better exercise the key generation
+    // and the interoperability check.
+    for i in 0..256 {
+        let rng = rand_chacha::ChaChaRng::seed_from_u64(i);
+        let msg = "Hello from the Raspberry Devs".as_bytes();
+        let (sig, pk) = frost_bluepallas::helper::generate_signature_random(msg, rng).unwrap();
+
+        helpers::verify_signature(msg, sig, pk);
+    }
+}
