d1300b8bbacd2b5b2201f2f51bdf8a66920849ee
scaraven <sc4raven@gmail.com>
2025-12-18 14:55:14 +0000

    test(mina-frost-client): add zkapp test vectors into integration tests

--- frost-bluepallas/src/transactions/zkapp_tx/zkapp_test_vectors.rs
+++ frost-bluepallas/src/transactions/zkapp_tx/zkapp_test_vectors.rs
@@ -8,6 +8,8 @@ use mina_hasher::Fp;
 use mina_signer::{CompressedPubKey, NetworkId};
 use std::str::FromStr;
 
+use crate::transactions::generic_tx::TransactionEnvelope;
+
 use super::{
     AccountPreconditions, AccountUpdate, AccountUpdateBody, ActionState, Actions, AuthRequired,
     Authorization, AuthorizationKind, BalanceChange, EpochData, EpochLedger, Events, FeePayer,
@@ -33,6 +35,12 @@ pub struct ZkAppTestVector {
     pub expected_account_updates_commitment: &'static str,
     /// Expected full commitment from zk_commit
     pub expected_full_commitment: &'static str,
+}
+
+impl From<ZkAppTestVector> for TransactionEnvelope {
+    fn from(vector: ZkAppTestVector) -> TransactionEnvelope {
+        TransactionEnvelope::new_zkapp(vector.network, vector.zkapp_command)
+    }
 }
 
 /// Additional test vectors specifically for hash_with_prefix function
