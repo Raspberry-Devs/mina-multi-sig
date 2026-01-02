254da819f0f2661af12961fa9e68e69ffabd3cde
scaraven <sc4raven@gmail.com>
2026-01-02 11:28:27 +0000

    refactor: use macro rule to reduce duplication

--- frost-bluepallas/src/transactions/zkapp_tx/zkapp_display.rs
+++ frost-bluepallas/src/transactions/zkapp_tx/zkapp_display.rs
@@ -9,7 +9,7 @@ use serde::Serialize;
 
 /// Helper to create a JSON-based Display implementation.
 /// Falls back to Debug formatting if serialization fails.
-pub fn json_display<T: Serialize + fmt::Debug>(
+pub(crate) fn json_display<T: Serialize + fmt::Debug>(
     value: &T,
     f: &mut fmt::Formatter<'_>,
 ) -> fmt::Result {
@@ -19,155 +19,52 @@ pub fn json_display<T: Serialize + fmt::Debug>(
     }
 }
 
+/// Macro to implement `Display` for types using JSON serialization.
+macro_rules! impl_json_display {
+    ($($ty:ty),* $(,)?) => {
+        $(
+            impl fmt::Display for $ty {
+                fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                    json_display(self, f)
+                }
+            }
+        )*
+    };
+}
+
 // -------------------------------------------------------------------------------------------------
 // --------------------------------- Composite Structs (JSON) --------------------------------------
 // -------------------------------------------------------------------------------------------------
 
-impl fmt::Display for ZKAppCommand {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
+impl_json_display!(
+    ZKAppCommand,
+    FeePayer,
+    FeePayerBody,
+    AccountUpdate,
+    AccountUpdateBody,
+    Update,
+    Permissions,
+    SetVerificationKey,
+    Preconditions,
+    AccountPreconditions,
+    NetworkPreconditions,
+    Events,
+    Actions,
+    Authorization,
+    VerificationKeyData,
+    TimingData,
+    EpochData,
+    EpochLedger,
+    AuthRequired,
+    MayUseToken,
+    BalanceChange,
+    AuthorizationKind,
+    ZkappUri,
+    TokenSymbol,
+);
 
-impl fmt::Display for FeePayer {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for FeePayerBody {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for AccountUpdate {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for AccountUpdateBody {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Update {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Permissions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for SetVerificationKey {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Preconditions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for AccountPreconditions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for NetworkPreconditions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Events {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Actions {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for Authorization {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
+// Generic impl for RangeCondition (cannot use macro due to generic bounds)
 impl<T: Serialize + fmt::Debug> fmt::Display for RangeCondition<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for VerificationKeyData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for TimingData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for EpochData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for EpochLedger {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for AuthRequired {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for MayUseToken {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for BalanceChange {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for AuthorizationKind {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for ZkappUri {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        json_display(self, f)
-    }
-}
-
-impl fmt::Display for TokenSymbol {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         json_display(self, f)
     }
@@ -192,15 +89,5 @@ impl fmt::Display for Field {
 impl fmt::Display for ActionState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
-    }
-}
-
-impl fmt::Display for AuthRequiredEncoded<bool> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{{ \"constant\": {}, \"signature_necessary\": {}, \"signature_sufficient\": {} }}",
-            self.constant, self.signature_necessary, self.signature_sufficient
-        )
     }
 }
